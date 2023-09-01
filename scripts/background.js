4605f9cafe6f5917a36169dcdfdca53ad7a98e4e
Hoang S Pham <pshoang102@gmail.com>
2023-09-01 01:06:06 -0500

    mvp v1.0

---
+++ scripts/background.js
@@ -0,0 +1,29 @@
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.storage.local.get(['blocked', 'enabled'], (local) => {
+    if (!Array.isArray(local.blocked)) {
+      chrome.storage.local.set({ blocked: [] });
+    }
+
+    if (typeof local.enabled !== 'boolean') {
+      chrome.storage.local.set({ enabled: false });
+    }
+  });
+});
+
+chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
+  const url = changeInfo.pendingUrl || changeInfo.url;
+  if (!url || !url.startsWith('http')) return;
+
+  const hostname = new URL(url).hostname;
+
+  chrome.storage.local.get(['blocked', 'enabled'], (local) => {
+    const { blocked, enabled } = local;
+    if (
+      Array.isArray(blocked) &&
+      enabled &&
+      blocked.find((domain) => hostname.includes(domain))
+    ) {
+      chrome.tabs.remove(tabId);
+    }
+  });
+});
