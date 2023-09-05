242b2dacc80a6110825b8caa365b0f302d823da3
Hoang S Pham <pshoang102@gmail.com>
2023-09-05 00:02:06 -0500

    re-styling and refactoring adding/deleting mechanisms

--- scripts/options.js
+++ scripts/options.js
@@ -1,13 +1,53 @@
-const textarea = document.getElementById('textarea');
+const textarea = document.getElementById('textInput');
 const save = document.getElementById('save');
 const checkbox = document.getElementById('checkbox');
 
-save.addEventListener('click', () => {
-  const blocked = textarea.value
-    .split('\n')
-    .map((s) => s.trim())
-    .filter(Boolean);
+const saveBlockedItems = () => {
+  /* Saving updated list to local storage */
+  const blacklist = document.getElementById('item-list');
+  let blocked = Array.from(blacklist.getElementsByTagName('li'));
+  blocked = blocked.map((item) => item.getAttribute('value'));
   chrome.storage.local.set({ blocked });
+};
+
+const deleteItem = (button) => {
+  button.parentNode.remove();
+  saveBlockedItems();
+};
+
+const addItem = (item = '') => {
+  let blacklist = document.getElementById('item-list');
+  let newItem = document.createElement('li');
+  // Set the text content of the new <li> element
+  newItem.textContent =
+    item.length === 0
+      ? textarea.value
+          .split('\n')
+          .map((s) => s.trim())
+          .filter(Boolean)
+      : item;
+  // Adds value to li element
+  newItem.setAttribute('value', newItem.textContent);
+  // Create a new "Delete" button for the new <li> element
+  var deleteButton = document.createElement('button');
+  deleteButton.textContent = 'Delete';
+  deleteButton.className = 'delete-button';
+  deleteButton.onclick = function () {
+    deleteItem(this);
+  };
+  // Append the "Delete" button to the new <li> element
+  newItem.appendChild(deleteButton);
+  // Append the new <li> element to the <ul> element
+  blacklist.appendChild(newItem);
+  // Save list if you're not loading list from local storage
+  if (item.length === 0) {
+    saveBlockedItems();
+  }
+};
+
+save.addEventListener('click', () => {
+  addItem();
+  textarea.value = '';
 });
 
 checkbox.addEventListener('change', (event) => {
@@ -19,7 +59,9 @@ window.addEventListener('DOMContentLoaded', () => {
   chrome.storage.local.get(['blocked', 'enabled'], function (local) {
     const { blocked, enabled } = local;
     if (Array.isArray(blocked)) {
-      textarea.value = blocked.join('\n');
+      blocked.forEach((item) => {
+        addItem(item);
+      });
       checkbox.checked = enabled;
     }
   });
